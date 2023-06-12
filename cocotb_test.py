c25e05dad713f4a8f0b22ef051bfc094e9e3258e
M0stafaRady <mostafa.rady@efabless.com>
2023-06-12 02:06:55 -0700

    update cocotb test with GUI

--- cocotb_test.py
+++ cocotb_test.py
@@ -1,16 +1,25 @@
 import cocotb 
 from cocotb.clock import Clock
 from cocotb.triggers import Edge, First, NextTimeStep
+import tkinter as tk
 
 
 @cocotb.test()
 async def cocotb_test(dut):
-    c = Clock(dut.clk, 50, 'ns')
+    c = Clock(dut.clk, 100, 'ns')
     await cocotb.start(c.start())
     await reset(dut)
     digit0 = digit1 = digit2 = digit3 = 0xFE
     change = True
-    for i in range(10000):
+    root = tk.Tk()
+    root.title("7 segment display")
+    screen = tk.Canvas(root)
+    screen.grid()
+    dig0 = Digit(screen, 10, 10) 
+    dig1 = Digit(screen, 40, 10) 
+    dig2 = Digit(screen, 70, 10) 
+    dig3 = Digit(screen, 100, 10)
+    while True:
         digit_num, digit = await read_seg(dut)
         if digit_num == 0 and digit0 != digit: 
             digit0 = digit
@@ -25,9 +34,17 @@ async def cocotb_test(dut):
             digit3 = digit
             change = True
         if change:
-            cocotb.log.info(f"digit num = {digit_num}")
-            cocotb.log.info(f"clock =  {int_to_seg(digit0)} {int_to_seg(digit1)} {int_to_seg(digit2)} {int_to_seg(digit3)}")
+            num =int_to_seg(digit0) + int_to_seg(digit1)*10 + int_to_seg(digit2)*100 + int_to_seg(digit3)*1000
+            dig0.show(int_to_seg(digit0))
+            dig1.show(int_to_seg(digit1))
+            dig2.show(int_to_seg(digit2))
+            dig3.show(int_to_seg(digit3))
+            cocotb.log.debug(f"digit num = {num}")
+            cocotb.log.debug(f"clock =  {int_to_seg(digit0)} {int_to_seg(digit1)} {int_to_seg(digit2)} {int_to_seg(digit3)}")
             change = False
+        if int_to_seg(digit3) == 9:
+            break
+        root.update()
 
 
 
@@ -71,3 +88,36 @@ def int_to_seg(digit):
         0xFB: 9,
     }[digit]    
     
+class Digit:
+    def __init__(self, canvas, x=10, y=10, length=20, width=4):
+        self.canvas = canvas
+        l = length
+        self.segs = []
+        offsets = offsets = (
+            (0, 0, 1, 0),  # top
+            (1, 0, 1, 1),  # upper right
+            (1, 1, 1, 2),  # lower right
+            (0, 2, 1, 2),  # bottom
+            (0, 1, 0, 2),  # lower left
+            (0, 0, 0, 1),  # upper left
+            (0, 1, 1, 1),  # middle
+        )
+        self.digits = (
+            (1, 1, 1, 1, 1, 1, 0),  # 0
+            (0, 1, 1, 0, 0, 0, 0),  # 1
+            (1, 1, 0, 1, 1, 0, 1),  # 2
+            (1, 1, 1, 1, 0, 0, 1),  # 3
+            (0, 1, 1, 0, 0, 1, 1),  # 4
+            (1, 0, 1, 1, 0, 1, 1),  # 5
+            (1, 0, 1, 1, 1, 1, 1),  # 6
+            (1, 1, 1, 0, 0, 0, 0),  # 7
+            (1, 1, 1, 1, 1, 1, 1),  # 8
+            (1, 1, 1, 1, 0, 1, 1),  # 9
+        )
+        for x0, y0, x1, y1 in offsets:
+            self.segs.append(canvas.create_line(
+                x + x0*l, y + y0*l, x + x1*l, y + y1*l,
+                width=width, state = 'hidden'))
+    def show(self, num):
+        for iid, on in zip(self.segs, self.digits[num]):
+            self.canvas.itemconfigure(iid, state = 'normal' if on else 'hidden')
